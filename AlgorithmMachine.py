048f7ff7b44bf97d40da9d01d0ac1c9578ba17ef
Erzc <84688309+Erzc@users.noreply.github.com>
2023-07-08 13:05:06 -0500

    Add main and first class files

---
+++ AlgorithmMachine.py
@@ -0,0 +1,27 @@
+# Programmer: Eric N
+# Purpose: Demonstrate use of different algorithms
+# Python version 3.10
+
+#imports BinarySearch.py file
+from BinarySearch import BinarySearch
+
+#--------------------#Main---------
+#Instantiate new object from class
+
+
+#Constructor without any arguments
+
+
+#Loop until user quits
+
+    #Ask for user problem/algorithm
+
+
+    #Exception errors
+
+
+
+
+
+
+print("You exited the program, goodbye!")
---
+++ BinarySearch.py
@@ -0,0 +1,4 @@
+
+
+class BinarySearch:
+    def __init__(self): #initializes the object’s state
