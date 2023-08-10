7a28c44c7bae2862fe597ad6d401b6a8d73521e4
Erzc <84688309+Erzc@users.noreply.github.com>
2023-08-10 17:43:35 -0600

    Added BFS to find all names ending with 'y'

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -1,2 +1,46 @@
+from collections import deque
+
 #class BreathFirstSearch:
 #    def __init__(self): #Initialize object’s state
+
+#Dictionary to represent the graph
+graph = {
+    "Abby": ["Emile"],
+    "Emile": ["Mike", "Ron"],
+    "Mike": [],
+    "Harry": ["Lewis"],
+    "Lewis": ["Carl"],
+    "Poe": ["Harry"],
+    "Ron": ["Poe", "Emile"],
+    "Carl": ["Abby", "Harry"]
+}
+
+def check_names(person_name):
+    return person_name[-1] == 'y'
+
+def search(name):
+    names_with_y = []
+    search_queue = deque() #Create a new queue
+    search_queue += graph[name] #Add the neighbors of the starting person
+    searched = [] #Keep track of visited nodes
+
+    while search_queue: #While queue isn't empty
+        person = search_queue.popleft()  #Get front node from the queue
+        if not person in searched: #Only search if person not already searched
+            if check_names(person):
+                names_with_y.append(person)
+            search_queue.extend(graph[person])
+            searched.append(person) #Mark person as searched
+
+    return names_with_y
+
+
+#Start searching from Abby
+names_ending_with_y = search("Abby")
+
+if names_ending_with_y:
+    print("Names that end with 'y':")
+    for name in names_ending_with_y:
+        print(name)
+else:
+    print("No names end with 'y'.")
