6bd08e508a6e8cc1ad140f09b0164007fcaaf35c
Erzc <84688309+Erzc@users.noreply.github.com>
2023-08-30 13:02:23 -0600

    Added Greedy Algorithm pseudo code

---
+++ GreedyAlgo.py
@@ -0,0 +1,26 @@
+
+
+#class GreedyAlgo:
+#    def __init__(self): #Initialize objects state
+
+
+def find_min_coins()
+    
+    #Sort coins from large to small
+
+    #Array for change
+
+
+    #For loop to iterate through coins array
+        #While loop as long as target is greater than coins
+            #Subtract coin
+
+    #Return change
+
+#Coins array - pennies, nickels, dimes, and quarters
+#Define target
+
+#Call find_min_coins function
+
+
+#Display results
