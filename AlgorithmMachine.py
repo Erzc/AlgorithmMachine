0ba9a19b02a3336f315eef35d12f4dfc546a83cc
Erzc <84688309+Erzc@users.noreply.github.com>
2023-09-19 13:05:07 -0600

    Display choices correctly and fixed exception handling

--- AlgorithmMachine.py
+++ AlgorithmMachine.py
@@ -15,42 +15,59 @@ from SelectionSort import SelectionSort as selectionSort
 
 #Loop until user quits
 while True:
-    #Try except statement to handle float value exception
+
+    #Display stories/choices
+    choices = '''
+    CHOICES:
+    1. Binary Search
+    2. Breadth First Search
+    3. Dijkstras Algorithm
+    4. Greedy Algorithm
+    5. Recursion
+    6. Selection Sort
+    '''
+
+    print(choices)
+
+    #Try except statement to handle value exception
     try:
         #Ask for user algorithm/problem
-        print("stories go here")
-        user_story = input("\nPlease select a story/algorithm to display (enter a number 1-6): ")
-
-        #Match-case statement, instantiate class object depending on answer
-        def match_result(x):
-            match x:
-                case '1':
-                    return binarySearch()
-                case '2':
-                    return breadthFirstSearch()
-                case '3':
-                    return dijkstrasAlgo()
-                case '4':
-                    return greedyAlgo()
-                case '5':
-                    return recursion()
-                case '6':
-                    return selectionSort()
-                case _:
-                    return "Error! Please enter a number 1-6."   #Default casea
-        
-        result_story = match_result(user_story)
-
-    #Exception handling
+        user_story = int(input("\nPlease select a story/algorithm to display (enter a number 1-6): "))
+        if user_story < 1 or user_story > 6:
+            raise ValueError("Your number must be between 1 and 6")
     except TypeError :
         print ("Error! Wrong data type.")
+        continue
     except NameError:
         print ("Error! Data name unknown.")
+        continue
     except Exception as e:
         print ("Error! Something went wrong...", e)
+        continue
+
+    #Match-case statement, instantiate class object depending on answer
+    def match_result(y):
+        match y:
+            case 1:
+                return binarySearch()
+            case 2:
+                return breadthFirstSearch()
+            case 3:
+                return dijkstrasAlgo()
+            case 4:
+                return greedyAlgo()
+            case 5:
+                return recursion()
+            case 6:
+                return selectionSort()
+            case _:
+                return "Error! Please enter a number 1-6."   #Default casea
     
-    again = input("\nDo another story (y/n)?")
-    if again == "n":
+    #Call 
+    result_story = match_result(user_story)
+
+    again = input("\nDo another story (y/n)?").strip().lower()
+    if again != "y":
         break
 
 
